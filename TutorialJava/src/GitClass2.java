a40431a900bcf5fa37667fe23d71bbfc45ac044d
srininv <srininv78@gmail.com>
2016-03-13 13:05:00 +0530

    Update GitClass2.java
    modified

--- TutorialJava/src/GitClass2.java
+++ TutorialJava/src/GitClass2.java
@@ -3,7 +3,7 @@ public class GitClass2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Second file has been added class2");
+System.out.println("Second file has been added class2111");
 	}
 
 }
